6ed7ef79bd34e51f0838da9c5d096b7ee160ab68
piripinui <philliprobinson@US-C02TW1DNHV2Q.local>
2019-01-08 14:02:20 +0000

    Use environment for access tokens

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,8 @@
 var edt;
 
 function init() {
-  edt = new edt3D('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiIzYTFhMTBlYi00YmNjLTRiNDItOWVhMi1lZWM0MTU2Njg3NzYiLCJpZCI6NTY4NSwic2NvcGVzIjpbImFzciIsImdjIl0sImlhdCI6MTU0MzkzODUwNX0.O-IRQpu6mqb7TjTYxW_Ut1kRz2Kolrl3ri7Y2KBYqDc',
-                       'cesiumContainer');
+  // Instantiate a new instance of the app.
+  $.get("/accessToken", function(data) {
+    edt = new edt3D(data, "cesiumContainer");
+  });
 }
