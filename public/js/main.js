acfa02764a5b6d1b747ac6cf0573e75ee0e0e464
piripinui <philliprobinson@US-C02TW1DNHV2Q.local>
2019-02-01 15:44:20 +0000

    Tweaks to cesium container sizing

--- public/js/main.js
+++ public/js/main.js
@@ -2,10 +2,14 @@ var edt;
 
 function init() {
   function correctWidgetHeight() {
+    // The Cesium container takes more height than is available to the screen, meaning you'll get scrollbars.
     var titleBarHeight = $("#abb_bar").outerHeight(true);
     var navbarHeight = $("#abb_navi").outerHeight(true);
-    var heightInPixels = $("#map_container").outerHeight(true) - titleBarHeight - navbarHeight;
-    console.log("Correcting height to " + heightInPixels);
+    var mapContainerHeight = $("#map_container").outerHeight(true);
+    var heightInPixels = mapContainerHeight - titleBarHeight - navbarHeight + 120;
+    var cesiumWidgetHeight = $(".cesium-widget").outerHeight(true);
+
+    console.log("Correcting height to " + heightInPixels + " from " + cesiumWidgetHeight);
     $(".cesium-widget").css("height", heightInPixels);
   }
   // Instantiate a new instance of the app.
